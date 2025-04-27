47cea050db120b962bb17cecdcd7a70922e666de
yura_filchakov <filchakov1993@gmail.com>
2025-04-27 23:28:26 +0700

    main.py

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+from app.controllers.chat_controller import router as chat_router
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+app.include_router(chat_router)
+
+templates = Jinja2Templates(directory='templates')
+
+
+@app.get('/', response_class=HTMLResponse)
+async def root(request: Request):
+    """Обработчик корневого маршрута. Возвращает HTML-страницу."""
+    return templates.TemplateResponse('index.html', {'request': request})
