69bfe77e9d9999b25b96f819c31570c221b89ad7
yura_filchakov <filchakov1993@gmail.com>
2025-05-04 02:36:15 +0700

    chat_controller.py

--- app/controllers/chat_controller.py
+++ app/controllers/chat_controller.py
@@ -1,16 +1,16 @@
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Depends
+from pydantic import ValidationError
 from starlette.responses import HTMLResponse
 from app.services.chat_service import ChatService
 from app.models.message import Message
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
 import json
+from app.services.chat_manager import ConnectionManager
 
 router = APIRouter()
-chat_service = ChatService()
 templates = Jinja2Templates(directory='templates')
-
-connected_users = {}
+connection_manager = ConnectionManager()
 
 
 @router.get('/', response_class=HTMLResponse)
@@ -20,32 +20,24 @@ async def get(request: Request):
 
 
 @router.websocket('/ws/{user_id}')
-async def websocket_endpoint(user_id: str, websocket: WebSocket):
+async def websocket_endpoint(user_id: str, websocket: WebSocket, chat_service: ChatService = Depends(ChatService)):
     """Обработчик WebSocket-соединения для пользователей чата."""
-    await websocket.accept()
-    connected_users[user_id] = websocket
+    await connection_manager.connect(user_id, websocket)
     try:
         while True:
             data = await websocket.receive_text()
             message_data = json.loads(data)
-
-            message_data['user_id'] = user_id
-            message_data['message_type'] = message_data.get('type', 'text')
-
+            
             message = Message(**message_data)
             
             if message.message_type == 'text':
                 filtered_message = chat_service.filter_profanity(message.content)
-                await broadcast(f'{user_id}: {filtered_message}')
+                await connection_manager.broadcast(f'{user_id}: {filtered_message}')
             elif message.message_type == 'file':
                 file_link = (f'<a href="data:application/octet-stream;base64,{message.file_content}" '
                              f'download="{message.filename}">Скачать {message.filename}</a>')
-                await broadcast(file_link)
+                await connection_manager.broadcast(file_link)
     except WebSocketDisconnect:
-        del connected_users[user_id]
-
-
-async def broadcast(message: str):
-    """Рассылает сообщение всем подключенным пользователям."""
-    for connection in connected_users.values():
-        await connection.send_text(message)
+        connection_manager.disconnect(user_id)
+    except ValidationError as e:
+        await websocket.send_text(f'Ошибка: {e.json()}')
