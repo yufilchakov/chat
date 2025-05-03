56fb42656bd7abab8a31db009498e53259dc577d
yura_filchakov <filchakov1993@gmail.com>
2025-05-04 02:36:56 +0700

    message.py

--- app/models/message.py
+++ app/models/message.py
@@ -1,11 +1,10 @@
-from pydantic import BaseModel
-from typing import Optional
+from pydantic import BaseModel, Field
 
 
 class Message(BaseModel):
     """Модель данных для сообщения в чате."""
     user_id: str
     content: str
-    message_type: str
-    filename: Optional[str] = None
-    file_content: Optional[str] = None
+    message_type: str = Field(default='text', alias='type')
+    filename: str | None = None
+    file_content: str | None = None
