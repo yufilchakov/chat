f07fcbe9bfaef565975b3d42d010ff9027bad1e0
yura_filchakov <filchakov1993@gmail.com>
2025-05-04 15:15:47 +0700

    chat_controller.py

--- app/controllers/chat_controller.py
+++ app/controllers/chat_controller.py
@@ -7,6 +7,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi import Request
 import json
 from app.services.chat_manager import ConnectionManager
+from app.depends import get_connection_manager
 
 router = APIRouter()
 templates = Jinja2Templates(directory='templates')
@@ -20,9 +21,14 @@ async def get(request: Request):
 
 
 @router.websocket('/ws/{user_id}')
-async def websocket_endpoint(user_id: str, websocket: WebSocket, chat_service: ChatService = Depends(ChatService)):
+async def websocket_endpoint(
+        user_id: str,
+        websocket: WebSocket,
+        chat_service: ChatService = Depends(ChatService),
+        conn_manager: ConnectionManager = Depends(get_connection_manager)
+):
     """Обработчик WebSocket-соединения для пользователей чата."""
-    await connection_manager.connect(user_id, websocket)
+    await conn_manager.connect(user_id, websocket)
     try:
         while True:
             data = await websocket.receive_text()
@@ -32,12 +38,12 @@ async def websocket_endpoint(user_id: str, websocket: WebSocket, chat_service: C
             
             if message.message_type == 'text':
                 filtered_message = chat_service.filter_profanity(message.content)
-                await connection_manager.broadcast(f'{user_id}: {filtered_message}')
+                await conn_manager.broadcast(f'{user_id}: {filtered_message}')
             elif message.message_type == 'file':
                 file_link = (f'<a href="data:application/octet-stream;base64,{message.file_content}" '
                              f'download="{message.filename}">Скачать {message.filename}</a>')
-                await connection_manager.broadcast(file_link)
+                await conn_manager.broadcast(file_link)
     except WebSocketDisconnect:
-        connection_manager.disconnect(user_id)
+        conn_manager.disconnect(user_id)
     except ValidationError as e:
         await websocket.send_text(f'Ошибка: {e.json()}')
